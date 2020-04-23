151b9ae9efa4910ab029564a640326a47d1a6e6c
Giorgio Costa <gegiorgio1991@gmail.com>
2020-04-22 23:38:45 -0300

    route em areas

--- Modulos/Produtos/Controllers/CadastroController.cs
+++ Modulos/Produtos/Controllers/CadastroController.cs
@@ -2,10 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace app_mvc_core.Modulos.Produtos.Controllers
 {
+    [Area("Produtos")]
+    [Route("produtos")]
     public class CadastroController : Controller
     {
-        [Area("Produtos")]
+
+        [Route("listar")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("buscar")]
+        public IActionResult Buscar()
         {
             return View();
         }

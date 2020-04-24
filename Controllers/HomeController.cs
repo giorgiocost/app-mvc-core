38188a77ac31d6cb5a658e5b02be52c83d00d533
Giorgio Costa <gegiorgio1991@gmail.com>
2020-04-23 23:59:25 -0300

    usando injeção de dependecia e solução para legado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,28 @@
+using app_mvc_core.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace app_mvc_core.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IPedidosRepository _pedidosRepository;
+
+        public HomeController(IPedidosRepository pedidosRepository)
         {
+            _pedidosRepository = pedidosRepository;
+        }
+        public IActionResult Index()
+        {            
+            var pedidos = _pedidosRepository.ObterPedido();
+            
+            return View();
+        }
+
+        // quando não for possivel injectar direto no construtor
+        public IActionResult ResolvendoLegado([FromServices] IPedidosRepository pedidosRepository)
+        {
+            var pedidos = _pedidosRepository.ObterPedido();
+            
             return View();
         }
     }
